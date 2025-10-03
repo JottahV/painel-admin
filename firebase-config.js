7c333bb99f499d3735178352a95738230fbfdcd1
JottahV <joaomendessouza15@gmail.com>
2025-10-03 12:59:04 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,16 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.
 
 // SEU CÓDIGO DE CONFIGURAÇÃO FICA AQUI, NO ÚNICO LUGAR CORRETO
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_CORRETA",
-  authDomain: "SEU_AUTH_DOMAIN_CORRETO",
-  projectId: "SEU_PROJECT_ID_CORRETO",
-  storageBucket: "SEU_STORAGE_BUCKET_CORRETO",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID_CORRETO",
-  appId: "SEU_APP_ID_CORRETO"
-};
+  apiKey: "AIzaSyAD4FpqCB-IC-Cp6Fz-lDEq5NsMMbf-Iy4",
+  authDomain: "controle-de-ponto-e260123.firebaseapp.com",
+  projectId: "controle-de-ponto-e260123",
+  storageBucket: "controle-de-ponto-e260123.firebasestorage.app",
+  messagingSenderId: "1055982400261",
+  appId: "1:1055982400261:web:af81ad9dfce100f3867d6c"
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
 // Disponibiliza a autenticação para os outros arquivos usarem
+
 export const auth = getAuth(app);
